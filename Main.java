5ac8e93544ff035e7ad961090bb389c99342515f
kuehnma79269 <kuehnma79269@th-nuernberg.de>
2020-10-23 09:50:10 +0000

    add function zahlenfilter

--- Main.java
+++ Main.java
@@ -1,5 +1,22 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello Matia!");
+    zahlenfilter();
   }
+
+  public static void zahlenfilter() {
+    for (int i = 0; i<200; i++) {
+      if (i % 5 == 0 && i != 0) {
+        System.out.println(i + " ist durch 5 teilbar");
+      } 
+      if ((i-9)%10) == 0 && i != 0) {
+        System.out.println("Zahl endet auf 9");
+      }
+      if (((i + (i-1)) % 3) == 0 && i != 0) {
+        System.out.println( i + " und " + (i-1) + " addiert ergeben " + (i+(i-1)) + " und " + (i+(i-1)) + " ist durch 3 teilbar" );
+      }
+    }
+  }
+
+
 }
